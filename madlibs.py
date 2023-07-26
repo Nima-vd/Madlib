60dfbb4b59e691a621a7ab8e49825b7a95540d5b
Nima-vd <121560950+Nima-vd@users.noreply.github.com>
2023-07-26 13:53:07 +0545

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,11 @@
+adj = input("adjective:")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+verb3 = input("Verb: ")
+player = input("Player: ")
+famous_person = input("famous person: ")
+
+madlib = f"Computer programming is so {adj}! It makes me so excited all the time because I love to {verb1}.Stay hydrated and {verb2} like you are {famous_person}.\
+My favourite player is{player} and he is very skillful"
+
+print(madlib)
